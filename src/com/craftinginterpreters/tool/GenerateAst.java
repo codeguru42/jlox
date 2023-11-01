7d36cacd6c903620920a977403a1defe7ce1fd61
Code-Apprentice <codeguru42@gmail.com>
2023-10-31 20:43:39 -0600

    Generate some expression classes for the AST

--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -36,7 +36,37 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        // The AST classes
+        for (String type : types) {
+            String[] tokens = type.split(":");
+            String className = tokens[0].trim();
+            String fields = tokens[1].trim();
+            defineType(writer, baseName, className, fields);
+        }
+
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+        writer.println("    static class " + className + " extends " + baseName + " {");
+
+        // Constructor
+        writer.println("        " + className + "(" + fieldList + ") {");
+        String[] fields = fieldList.split(", ");
+        for (String field : fields) {
+            String name = field.split(" ")[1];
+            writer.println("            this." + name + " = " + name + ";");
+        }
+
+        writer.println("        }");
+
+        // Fields
+        writer.println();
+        for (String field : fields) {
+            writer.println("        final " + field + ";");
+        }
+
+        writer.println("    }");
     }
 }
