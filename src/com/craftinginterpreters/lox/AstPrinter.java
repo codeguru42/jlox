6ae5bb45d68e9e60e1b8a5db7e075d6768d46da5
Code-Apprentice <codeguru42@gmail.com>
2023-11-07 00:03:17 -0700

    Pretty printer prints variable name

--- src/com/craftinginterpreters/lox/AstPrinter.java
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -1,6 +1,21 @@
 package com.craftinginterpreters.lox;
 
 public class AstPrinter implements Expr.Visitor<String> {
+    public static void main(String[] args) {
+        Expr expression = new Expr.Binary(
+            new Expr.Unary(
+                new Token(TokenType.MINUS, "-", null, 1),
+                new Expr.Literal(123)
+            ),
+            new Token(TokenType.STAR, "*", null, 1),
+            new Expr.Grouping(
+                new Expr.Literal(45.67)
+            )
+        );
+
+        System.out.println(new AstPrinter().print(expression));
+    }
+
     String print(Expr expr) {
         return expr.accept(this);
     }
@@ -26,6 +41,11 @@ public class AstPrinter implements Expr.Visitor<String> {
         return parenthesize(expr.operator.lexeme, expr.right);
     }
 
+    @Override
+    public String visitVariableExpr(Expr.Variable expr) {
+        return expr.name.lexeme;
+    }
+
     private String parenthesize(String name, Expr... exprs) {
         StringBuilder builder = new StringBuilder();
 
@@ -36,20 +56,5 @@ public class AstPrinter implements Expr.Visitor<String> {
         }
         builder.append(")");
         return builder.toString();
-    }
-
-    public static void main(String[] args) {
-        Expr expression = new Expr.Binary(
-            new Expr.Unary(
-                new Token(TokenType.MINUS, "-", null, 1),
-                new Expr.Literal(123)
-            ),
-            new Token(TokenType.STAR, "*", null, 1),
-            new Expr.Grouping(
-                new Expr.Literal(45.67)
-            )
-        );
-
-        System.out.println(new AstPrinter().print(expression));
     }
 }
