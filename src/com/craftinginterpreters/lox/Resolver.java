247b1586e2dabdce616a6482315ca5df82fa73d4
Code-Apprentice <codeguru42@gmail.com>
2023-12-02 11:57:46 -0700

    Error handling for return in init()

--- src/com/craftinginterpreters/lox/Resolver.java
+++ src/com/craftinginterpreters/lox/Resolver.java
@@ -119,6 +119,9 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
         for (Stmt.Function method : stmt.methods) {
             FunctionType declaration = FunctionType.METHOD;
+            if (method.name.lexeme.equals("init")) {
+                declaration = FunctionType.INITIALIZER;
+            }
             resolveFunction(method, declaration);
         }
 
@@ -164,6 +167,10 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         }
 
         if (stmt.value != null) {
+            if (currentFunction == FunctionType.INITIALIZER) {
+                Lox.error(stmt.keyword, "Can't return a value from an initializer.");
+            }
+
             resolve(stmt.value);
         }
         return null;
@@ -250,6 +257,7 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     private enum FunctionType {
         NONE,
         FUNCTION,
+        INITIALIZER,
         METHOD,
 
     }
